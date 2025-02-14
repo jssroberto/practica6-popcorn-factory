c5a159886cac85f26d86093d8a513c03aff9b2d4
Roberto Garcia <159673089+jssroberto@users.noreply.github.com>
2025-02-13 23:14:59 -0700

    Popcorn Factory finished
    Good night

---
+++ PopCornFactory244913/app/src/main/java/garcia/roberto/popcornfactory244913/DetallePelicula.kt
@@ -0,0 +1,34 @@
+package garcia.roberto.popcornfactory244913
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_pelicula)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bundle = intent.extras
+
+        if (bundle != null) {
+            val titulo = bundle.getString("titulo")
+            val header = bundle.getInt("header")
+            val sinopsis = bundle.getString("sinopsis")
+
+            findViewById<TextView>(R.id.txt_titulo).text = titulo
+            findViewById<TextView>(R.id.txt_sinopsis).text = sinopsis
+            findViewById<ImageView>(R.id.img_pelicula).setImageResource(header)
+        }
+    }
+
+}
